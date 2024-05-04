5fcbbd9b6049d52969fa7e1f4318d4ea52b4308b
Omkar Kawale <121446208+Omkar1643-om@users.noreply.github.com>
2024-05-04 14:38:52 +0530

    first Commit Done

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Header from './components/Header/Header.js';
+import Body from './components/Body/Body.js';
+import Footer1 from './components/Footer/Footer1.js';
+import Footer2 from './components/Footer/Footer2.js';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Body />
+      <Footer1 />
+      <Footer2 />
+    </>
+  );
+}
+
+export default App;
